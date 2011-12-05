f30b98c7502ade4b8afbc0b2b65d94f269012245
Moritz Winter <moritz.a.winter@googlemail.com>
2011-12-06 00:28:41 +0100

    Static pages home, contact, about added

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -16,4 +16,11 @@ describe PagesController do
     end
   end
 
+  describe "Get 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+  end
+
 end
