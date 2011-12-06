000803fb7148128c4265308f829209f69c8ac9f8
Moritz Winter <moritz.a.winter@googlemail.com>
2011-12-06 01:15:51 +0100

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
-  def home
+  def home    #home action
+    @title = "Home"
   end
 
-  def contact
+  def contact #contact action
+    @title = "Contact"
   end
 
-  def about
+  def about   #about action
+    @title = "About"
   end
 
 end
